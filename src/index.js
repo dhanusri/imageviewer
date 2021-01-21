5fc1cbc4ec0867cb39eff03ee01ab472aded4b2f
Vipin P K <51770037+itsvipinpk@users.noreply.github.com>
2021-01-21 20:05:49 +0530

    Updated react and react dom versions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
     document.getElementById('root')
 );
+
+serviceWorker.unregister();
